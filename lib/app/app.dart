065c4ba29a280aeb334d97c51006d1b9aac163ff
Fawzi Gharib <fawz11gharib@gmail.com>
2024-02-16 20:09:59 +0300

    initial commit

---
+++ lib/app/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+
+  // named constructor
+  const MyApp._internal();
+
+  static const MyApp _instance = MyApp._internal(); // singleton or single instance
+
+  factory MyApp() => _instance; // factory
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
