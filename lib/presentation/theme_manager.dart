2a38377d214273df4a3c93c4405ef980b1e75f33
Fawzi Gharib <fawz11gharib@gmail.com>
2024-02-16 21:12:11 +0300

    Theme Manager : Card Theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:tut_app/presentation/color_manager.dart';
+import 'package:tut_app/presentation/values_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-    // Main colors
-    primaryColor: ColorManager.primary,
-    primaryColorLight: ColorManager.lightPrimary,
-    primaryColorDark: ColorManager.darkPrimary,
-    disabledColor: ColorManager.grey1,
+      // Main colors
+      primaryColor: ColorManager.primary,
+      primaryColorLight: ColorManager.lightPrimary,
+      primaryColorDark: ColorManager.darkPrimary,
+      disabledColor: ColorManager.grey1,
 
-    // Card view theme
+      // Card view theme
+      cardTheme: CardTheme(
+        color: ColorManager.white,
+        shadowColor: ColorManager.grey,
+        elevation: AppSize.s4,
+      )
 
-    // App bar theme
+      // App bar theme
 
-    // Button theme
+      // Button theme
 
-    // Text Theme
+      // Text Theme
 
-    // Input decoration
-  );
+      // Input decoration
+      );
 }
