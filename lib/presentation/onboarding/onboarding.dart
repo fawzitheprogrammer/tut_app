e391c5c6d0e94dc12d11e1635870f5ae4476178c
Fawzi Gharib <fawz11gharib@gmail.com>
2024-02-17 01:14:49 +0300

    OnBoarding Part 1

--- lib/presentation/onboarding/onboarding.dart
+++ lib/presentation/onboarding/onboarding.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:tut_app/presentation/resources/asstes_manager.dart';
+import 'package:tut_app/presentation/resources/color_manager.dart';
+import 'package:tut_app/presentation/resources/strings_manager.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({super.key});
@@ -8,12 +12,65 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
+  late final List<SliderObject> _list = _getSliderData();
+  final PageController _pageController = PageController();
+  int _currentIndex = 0;
+
+  List<SliderObject> _getSliderData() => [
+        SliderObject(
+          title: AppStrings.onBoardingTitle1,
+          subtitle: AppStrings.onBoardingSubTitle1,
+          image: Assets.onboardingLogo1,
+        ),
+        SliderObject(
+          title: AppStrings.onBoardingTitle2,
+          subtitle: AppStrings.onBoardingSubTitle2,
+          image: Assets.onboardingLogo2,
+        ),
+        SliderObject(
+          title: AppStrings.onBoardingTitle3,
+          subtitle: AppStrings.onBoardingSubTitle3,
+          image: Assets.onboardingLogo3,
+        ),
+        SliderObject(
+          title: AppStrings.onBoardingTitle4,
+          subtitle: AppStrings.onBoardingSubTitle4,
+          image: Assets.onboardingLogo4,
+        ),
+      ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Welcome'),
+    return Scaffold(
+      appBar: AppBar(
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: ColorManager.white,
+          statusBarBrightness: Brightness.dark,
+        ),
+      ),
+      body: PageView.builder(
+        controller: _pageController,
+        itemCount: _list.length,
+        onPageChanged: (value) {
+          setState(() {
+            if (mounted) {
+              setState(() {
+                _currentIndex = value;
+              });
+            }
+          });
+        },
+        itemBuilder: (context, index) => const Center(),
       ),
     );
   }
+}
+
+class SliderObject {
+  final String title;
+  final String subtitle;
+  final String image;
+
+  SliderObject(
+      {required this.title, required this.subtitle, required this.image});
 }
--- lib/presentation/resources/asstes_manager.dart
+++ lib/presentation/resources/asstes_manager.dart
@@ -2,4 +2,9 @@ const String imageFolderPath = 'assets/images';
 
 class Assets {
   static const String splashLogo = '$imageFolderPath/splash_logo.png';
+
+  static const String onboardingLogo1 = "$imageFolderPath/onboarding_logo1.svg";
+  static const String onboardingLogo2 = "$imageFolderPath/onboarding_logo2.svg";
+  static const String onboardingLogo3 = "$imageFolderPath/onboarding_logo3.svg";
+  static const String onboardingLogo4 = "$imageFolderPath/onboarding_logo4.svg";
 }
